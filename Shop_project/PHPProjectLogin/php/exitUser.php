eff1a1d4b7b4dc982cadd7d3b9439e1f81a04c3a
Gaukhar11188 <146077490+Gaukhar11188@users.noreply.github.com>
2023-12-21 16:09:00 +0600

    Delete Shop_project/PHPProjectLogin directory

--- Shop_project/PHPProjectLogin/php/exitUser.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-
-echo '<li><a class="nav-link" href="customer_login.html"><img src="images/user.svg"></a></li>';
-echo '<li><a class="nav-link" href="cart.html"><img src="images/cart.svg"></a></li>';
-
-?>
